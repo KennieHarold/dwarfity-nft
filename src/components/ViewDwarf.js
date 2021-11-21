1c8e63bab286e62a4c133ae2c81f00cc88c1dbc4
Kennie Harold <kennie.harold@gmail.com>
2021-11-22 00:34:55 +0800

    Integrate view dwarf

--- src/components/ViewDwarf.js
+++ src/components/ViewDwarf.js
@@ -1,7 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { Card, Container, Spinner } from 'react-bootstrap';
+import { CoreServiceInstance } from '../services';
 
 function ViewDwarf() {
-  return <div>Heeey</div>;
+  const [dwarf, setDwarf] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    async function init() {
+      setLoading(true);
+
+      const path = location.pathname.split('/');
+      const tokenId = path[path.length - 1];
+      const dbDwarf = await CoreServiceInstance.getDwarfityByTokenId(tokenId);
+
+      if (dbDwarf && dbDwarf !== undefined) {
+        setDwarf(dbDwarf);
+      }
+
+      setLoading(false);
+    }
+    init();
+  }, []);
+  return (
+    <Container className="mt-5">
+      <div className="d-flex align-items-center justify-content-center">
+        {loading ? (
+          <Spinner animation="grow" variant="primary" />
+        ) : dwarf ? (
+          <Card style={{ width: '18rem', cursor: 'pointer' }}>
+            <Card.Img variant="top" src={dwarf?.image} />
+            <Card.Body>
+              <Card.Text
+                className="text-secondary fw-bold"
+                style={{ padding: 0, margin: 0 }}
+              >
+                {dwarf?.name}
+              </Card.Text>
+              <Card.Text className="fw-bold">#{dwarf?.token_id}</Card.Text>
+            </Card.Body>
+          </Card>
+        ) : (
+          <h4>Not Found!</h4>
+        )}
+      </div>
+    </Container>
+  );
 }
 
 export default ViewDwarf;
